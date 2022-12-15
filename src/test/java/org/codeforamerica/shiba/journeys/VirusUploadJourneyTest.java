fc889beea2f90a34732b71152faac701ffc0665d
Taylor Johnson <taylor.johnson@state.mn.us>
2022-12-15 16:27:35 -0600

    Write/modify tests (they should fail at this point).
    [#182620523]

---
+++ src/test/java/org/codeforamerica/shiba/journeys/VirusUploadJourneyTest.java
@@ -0,0 +1,82 @@
+package org.codeforamerica.shiba.journeys;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.springframework.web.multipart.MultipartFile;
+
+@Tag("documentUploadJourney")
+public class VirusUploadJourneyTest extends JourneyTest {
+
+  @Test
+  void whenDocumentUploadVirusThereShouldBeAnError() throws InterruptedException, IOException {
+    getToDocumentUploadScreen();
+    uploadGIFVirusFile();
+    waitForErrorMessage();
+    assertThat(driver.findElements(By.className("text--error")).get(0).getText()).contains(
+        "virus");  //update with virus error message
+    assertThat(driver.findElement(By.id("number-of-uploaded-files")).getText()).contains(
+        "0 files added");
+    assertThat(driver.findElement(By.id("submit-my-documents")).getAttribute("class"))
+    	.contains("hidden");
+    testPage.clickLink("remove");
+
+    uploadJPGVirusFile();
+    waitForErrorMessage();
+    assertThat(driver.findElements(By.className("text--error")).get(0).getText()).contains(
+        "virus");  //update with virus error message
+    assertThat(driver.findElement(By.id("number-of-uploaded-files")).getText()).contains(
+        "0 files added");
+    assertThat(driver.findElement(By.id("submit-my-documents")).getAttribute("class"))
+    	.contains("hidden");
+    testPage.clickLink("remove");
+
+    uploadPNGVirusFile();
+    waitForErrorMessage();
+    assertThat(driver.findElements(By.className("text--error")).get(0).getText()).contains(
+        "virus");  //update with virus error message
+    assertThat(driver.findElement(By.id("number-of-uploaded-files")).getText()).contains(
+        "0 files added");
+    assertThat(driver.findElement(By.id("submit-my-documents")).getAttribute("class"))
+    	.contains("hidden");
+    testPage.clickLink("remove");
+    
+    uploadPDFVirusFile1();
+    waitForErrorMessage();
+    assertThat(driver.findElements(By.className("text--error")).get(0).getText()).contains(
+        "virus");  //update with virus error message
+    assertThat(driver.findElement(By.id("number-of-uploaded-files")).getText()).contains(
+        "0 files added");
+    assertThat(driver.findElement(By.id("submit-my-documents")).getAttribute("class"))
+    	.contains("hidden");
+    testPage.clickLink("remove");
+
+    uploadPDFVirusFile2();
+    waitForErrorMessage();
+    assertThat(driver.findElements(By.className("text--error")).get(0).getText()).contains(
+        "virus");  //update with virus error message
+    assertThat(driver.findElement(By.id("number-of-uploaded-files")).getText()).contains(
+        "0 files added");
+    assertThat(driver.findElement(By.id("submit-my-documents")).getAttribute("class"))
+    	.contains("hidden");
+    testPage.clickLink("remove");
+    
+    uploadPDFVirusFile3();
+    waitForErrorMessage();
+    assertThat(driver.findElements(By.className("text--error")).get(0).getText()).contains(
+        "virus");  //update with virus error message
+    assertThat(driver.findElement(By.id("number-of-uploaded-files")).getText()).contains(
+        "0 files added");
+    assertThat(driver.findElement(By.id("submit-my-documents")).getAttribute("class"))
+    	.contains("hidden");
+    testPage.clickLink("remove");
+  }
+}
