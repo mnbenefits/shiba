d231954fc15b1dd36f73edb47c326f61a0c4b854
EricMN35Johnson <eric.m.johnson@state.mn.us>
2024-01-12 14:46:32 -0600

    Added test for Becker and WEN.
    Co-authored-by: John Bisek <john.bisek@state.mn.us>

--- src/test/java/org/codeforamerica/shiba/pages/events/ApplicationSubmittedListenerTest.java
+++ src/test/java/org/codeforamerica/shiba/pages/events/ApplicationSubmittedListenerTest.java
@@ -1,6 +1,10 @@
 package org.codeforamerica.shiba.pages.events;
 
+import static org.codeforamerica.shiba.County.Becker;
+import static org.codeforamerica.shiba.County.Olmsted;
 import static org.codeforamerica.shiba.County.Ramsey;
+import static org.codeforamerica.shiba.TribalNation.RedLakeNation;
+import static org.codeforamerica.shiba.TribalNation.WhiteEarthNation;
 import static org.codeforamerica.shiba.output.Recipient.CLIENT;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
@@ -13,14 +17,13 @@ import static org.mockito.Mockito.when;
 import java.sql.Timestamp;
 import java.time.ZonedDateTime;
 import java.util.List;
-import java.util.Collection;
-import java.util.HashSet;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.Set;
 
 import org.codeforamerica.shiba.County;
 import org.codeforamerica.shiba.MonitoringService;
+import org.codeforamerica.shiba.TribalNationRoutingDestination;
 import org.codeforamerica.shiba.application.Application;
 import org.codeforamerica.shiba.application.ApplicationRepository;
 import org.codeforamerica.shiba.application.parsers.ContactInfoParser;
@@ -154,6 +157,97 @@ class ApplicationSubmittedListenerTest {
 			applicationSubmittedListener.notifyApplicationSubmission(event);
 			verify(communicationClient, times(1)).send(jsonObject);
 		}
+		
+		@Test
+		void shouldIncludeCountyAndTribalNationRoutingInRestCall() {
+			ZonedDateTime dateTime = ZonedDateTime.now();
+			String applicationId = "applicationId";
+			ApplicationData applicationData = mock(ApplicationData.class);
+			when(applicationData.getOriginalCounty()).thenReturn("Olmsted");
+			when(applicationData.getId()).thenReturn(applicationId);
+			when(applicationData.getPagesData())
+					.thenReturn(new PagesDataBuilder().withPageData("contactInfo", "phoneOrEmail", "TEXT")
+							.withPageData("identifyCounty", "county", "Olmsted")
+							.withPageData("selectTheTribe", "selectedTribe", "Red Lake Nation")
+							.withPageData("contactInfo", "phoneNumber", "(651)555-5555")
+							.withPageData("languagePreferences", "spokenLanguage", "English")
+							.withPageData("languagePreferences", "writtenLanguage", "English")
+							.withPageData("personalInfo", "lastName", "LastName")
+							.withPageData("personalInfo", "firstName", "FirstName").build());
+			Application application = Application.builder().id(applicationId).county(County.Olmsted)
+					.completedAt(dateTime).applicationData(applicationData).build();
+			ApplicationSubmittedEvent  event = new ApplicationSubmittedEvent("someSessionId", applicationId, null, Locale.ENGLISH);
+			when(applicationSubmittedListener.getApplicationFromEvent(event)).thenReturn(application);
+			when(routingDecisionService.getRoutingDestinationByName("Olmsted")).thenReturn(new CountyRoutingDestination(Olmsted, "DPI", "email", "(651)555-5555"));
+			Set<RoutingDestination> destinationSet = Set.of(new CountyRoutingDestination(Olmsted, "DPI", "email", "(651)555-5555"), new TribalNationRoutingDestination(RedLakeNation, "DPI", "email", "(651)555-6666"));
+			when(routingDecisionService.findRoutingDestinations(application)).thenReturn(destinationSet);
+			JsonObject jsonObject = new JsonObject();
+			jsonObject.addProperty("appId", applicationData.getId());
+			jsonObject.addProperty("expedited", applicationData.getExpeditedEligibility().toString());
+			jsonObject.addProperty("firstName", ContactInfoParser.firstName(applicationData));
+			jsonObject.addProperty("lastName", ContactInfoParser.lastName(applicationData));
+			jsonObject.addProperty("phoneNumber", ContactInfoParser.phoneNumber(applicationData).replaceAll("[^0-9]", ""));
+			jsonObject.addProperty("email", ContactInfoParser.email(applicationData));
+			jsonObject.addProperty("opt-status-sms", ContactInfoParser.optedIntoTEXT(applicationData));
+			jsonObject.addProperty("opt-status-email", ContactInfoParser.optedIntoEmailCommunications(applicationData));
+			jsonObject.addProperty("writtenLangPref", ContactInfoParser.writtenLanguagePref(applicationData));
+			jsonObject.addProperty("spokenLangPref", ContactInfoParser.spokenLanguagePref(applicationData));
+			jsonObject.addProperty("completed-dt", Timestamp.valueOf(dateTime.toLocalDateTime()).toString()); 
+			jsonObject.addProperty("county", "Olmsted");
+			jsonObject.addProperty("countyPhoneNumber", "(651)555-5555");
+			jsonObject.addProperty("countyRoutingDestination", "Olmsted");
+			jsonObject.addProperty("countyRoutingPhoneNumber", "(651)555-5555");
+			jsonObject.addProperty("tribalNationRoutingDestination", "Red Lake Nation");
+			jsonObject.addProperty("tribalNationRoutingPhoneNumber", "(651)555-6666");
+			when(communicationClient.isEnabled()).thenReturn(true);
+			applicationSubmittedListener.notifyApplicationSubmission(event);
+			verify(communicationClient, times(1)).send(jsonObject);
+		}	
+		
+		@Test
+		void shouldOnlyIncludeCountyForBeckerAndWEN() {
+			ZonedDateTime dateTime = ZonedDateTime.now();
+			String applicationId = "applicationId";
+			ApplicationData applicationData = mock(ApplicationData.class);
+			when(applicationData.getOriginalCounty()).thenReturn("Becker");
+			when(applicationData.getId()).thenReturn(applicationId);
+			when(applicationData.getPagesData())
+					.thenReturn(new PagesDataBuilder().withPageData("contactInfo", "phoneOrEmail", "TEXT")
+							.withPageData("identifyCounty", "county", "Becker")
+							.withPageData("selectTheTribe", "selectedTribe", "White Earth Nation")
+							.withPageData("contactInfo", "phoneNumber", "(651)555-5555")
+							.withPageData("languagePreferences", "spokenLanguage", "English")
+							.withPageData("languagePreferences", "writtenLanguage", "English")
+							.withPageData("personalInfo", "lastName", "LastName")
+							.withPageData("personalInfo", "firstName", "FirstName").build());
+			Application application = Application.builder().id(applicationId).county(County.Becker)
+					.completedAt(dateTime).applicationData(applicationData).build();
+			ApplicationSubmittedEvent  event = new ApplicationSubmittedEvent("someSessionId", applicationId, null, Locale.ENGLISH);
+			when(applicationSubmittedListener.getApplicationFromEvent(event)).thenReturn(application);
+			when(routingDecisionService.getRoutingDestinationByName("Becker")).thenReturn(new CountyRoutingDestination(Becker, "DPI", "email", "(651)555-5555"));
+			Set<RoutingDestination> destinationSet = Set.of(new TribalNationRoutingDestination(WhiteEarthNation, "DPI", "email", "(651)555-6666"));
+			when(routingDecisionService.findRoutingDestinations(application)).thenReturn(destinationSet);
+			JsonObject jsonObject = new JsonObject();
+			jsonObject.addProperty("appId", applicationData.getId());
+			jsonObject.addProperty("expedited", applicationData.getExpeditedEligibility().toString());
+			jsonObject.addProperty("firstName", ContactInfoParser.firstName(applicationData));
+			jsonObject.addProperty("lastName", ContactInfoParser.lastName(applicationData));
+			jsonObject.addProperty("phoneNumber", ContactInfoParser.phoneNumber(applicationData).replaceAll("[^0-9]", ""));
+			jsonObject.addProperty("email", ContactInfoParser.email(applicationData));
+			jsonObject.addProperty("opt-status-sms", ContactInfoParser.optedIntoTEXT(applicationData));
+			jsonObject.addProperty("opt-status-email", ContactInfoParser.optedIntoEmailCommunications(applicationData));
+			jsonObject.addProperty("writtenLangPref", ContactInfoParser.writtenLanguagePref(applicationData));
+			jsonObject.addProperty("spokenLangPref", ContactInfoParser.spokenLanguagePref(applicationData));
+			jsonObject.addProperty("completed-dt", Timestamp.valueOf(dateTime.toLocalDateTime()).toString()); 
+			jsonObject.addProperty("county", "Becker");
+			jsonObject.addProperty("countyPhoneNumber", "(651)555-5555");
+			jsonObject.addProperty("tribalNationRoutingDestination", "White Earth Nation");
+			jsonObject.addProperty("tribalNationRoutingPhoneNumber", "(651)555-6666");
+			when(communicationClient.isEnabled()).thenReturn(true);
+			applicationSubmittedListener.notifyApplicationSubmission(event);
+			verify(communicationClient, times(1)).send(jsonObject);
+		}	
+	
 	}
 
 
