a16f96b1ddcbf05e67d98178aacb64cacb523b3f
Taylor Johnson <taylor.johnson@state.mn.us>
2024-03-11 14:05:16 -0500

    Added comment to Page Date for clarification

--- src/main/java/org/codeforamerica/shiba/pages/data/PageData.java
+++ src/main/java/org/codeforamerica/shiba/pages/data/PageData.java
@@ -48,6 +48,8 @@ public class PageData extends HashMap<String, InputData> {
           List<String> sanitizedValue = value.stream()
               .map(v -> v.replace("\u0000", ""))
               .collect(Collectors.toCollection(ArrayList::new));
+          // Whitelist that allows all Letters, Numbers, Punctuation, and Seperator Characters
+          // This was implemented to removed Emojis
           sanitizedValue = sanitizedValue.stream()
     		  .map(v -> v.replaceAll("[^\\p{L}\\p{N}\\p{P}\\p{Z}]", ""))
     		  .collect(Collectors.toCollection(ArrayList::new));
