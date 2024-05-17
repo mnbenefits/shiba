781cccb2f80877e7ee744838d841f11ee3ac1e24
abdumn <abdu.esmael@state.mn.us>
2024-05-17 11:36:08 -0500

    CCM 138664: Implement logic to write to CCAP PDF Question 4

--- src/main/java/org/codeforamerica/shiba/output/documentfieldpreparers/StudentFullNamePreparer.java
+++ src/main/java/org/codeforamerica/shiba/output/documentfieldpreparers/StudentFullNamePreparer.java
@@ -2,14 +2,18 @@ package org.codeforamerica.shiba.output.documentfieldpreparers;
 
 import static org.codeforamerica.shiba.output.FullNameFormatter.getListOfSelectedFullNames;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import org.codeforamerica.shiba.application.Application;
 import org.codeforamerica.shiba.output.Document;
 import org.codeforamerica.shiba.output.DocumentField;
 import org.codeforamerica.shiba.output.DocumentFieldType;
+import org.codeforamerica.shiba.output.FullNameFormatter;
 import org.codeforamerica.shiba.output.Recipient;
+import org.codeforamerica.shiba.pages.data.PageData;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,17 +21,32 @@ public class StudentFullNamePreparer implements DocumentFieldPreparer {
 
   @Override
   public List<DocumentField> prepareDocumentFields(Application application, Document document,
-      Recipient recipient) {
-    List<String> students = getListOfSelectedFullNames(application, "whoIsGoingToSchool",
-        "whoIsGoingToSchool");
-    List<String> children = getListOfSelectedFullNames(application, "childrenInNeedOfCare",
-        "whoNeedsChildCare");
+			Recipient recipient) {
+		List<String> students = getListOfSelectedFullNames(application, "whoIsGoingToSchool", "whoIsGoingToSchool");
+		List<String> children = getListOfSelectedFullNames(application, "childrenInNeedOfCare", "whoNeedsChildCare");
+		students.retainAll(children);
 
-    students.retainAll(children);
-    AtomicInteger i = new AtomicInteger(0);
-    return students.stream()
-        .map(fullName -> new DocumentField("whoIsGoingToSchool", "fullName",
-            List.of(fullName), DocumentFieldType.SINGLE_VALUE, i.getAndIncrement()))
-        .collect(Collectors.toList());
-  }
+		PageData pageData = application.getApplicationData().getPageData("schoolDetails");
+
+		if (pageData == null) {
+			return List.of();
+		}
+		List<String> schoolNames = pageData.get("schoolName").getValue();
+
+		List<DocumentField> result = new ArrayList<>();
+
+		for (int i = 0; i < students.size(); i++) {
+
+			String fullName = students.get(i);
+			String schoolName = schoolNames.get(i);
+
+			result.add(new DocumentField("whoIsGoingToSchool", "fullName", List.of(fullName),
+					DocumentFieldType.SINGLE_VALUE, i));
+			result.add(new DocumentField("schoolDetails", "schoolName", List.of(schoolName),
+					DocumentFieldType.SINGLE_VALUE, i));
+		}
+		return result;
+
+	}
+
 }
