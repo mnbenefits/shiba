8ee503a32fc856a177873581b3ae4d8c3e31ed28
pwjmb05 <john.bisek@state.mn.us>
2024-06-13 07:42:32 -0500

    CCM Task 141211: Create a new schoolGrade page to gather "grade" input - fix tests

--- src/test/java/org/codeforamerica/shiba/journeys/FullFlowJourneyTest.java
+++ src/test/java/org/codeforamerica/shiba/journeys/FullFlowJourneyTest.java
@@ -139,6 +139,9 @@ public class FullFlowJourneyTest extends JourneyTest {
     testPage.enter("schoolName", "ABC School");
     testPage.clickContinue();
     
+    // School grade?
+    testPage.clickContinue();
+    
     // School start date?
     testPage.clickContinue();
 
