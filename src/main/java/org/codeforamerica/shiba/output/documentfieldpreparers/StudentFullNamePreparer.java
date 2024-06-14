45c90b41a3dd0db5d56d4764f6994d20bf66a13a
abdumn <abdu.esmael@state.mn.us>
2024-06-14 07:49:45 -0500

    CCM 141221,141203,141775,141210,141220: Add the Acrobat labels and Populate the CCAP PDF for school start date field and school grade field

--- src/main/java/org/codeforamerica/shiba/output/documentfieldpreparers/StudentFullNamePreparer.java
+++ src/main/java/org/codeforamerica/shiba/output/documentfieldpreparers/StudentFullNamePreparer.java
@@ -25,25 +25,59 @@ public class StudentFullNamePreparer implements DocumentFieldPreparer {
 		List<String> students = getListOfSelectedFullNames(application, "whoIsGoingToSchool", "whoIsGoingToSchool");
 		List<String> children = getListOfSelectedFullNames(application, "childrenInNeedOfCare", "whoNeedsChildCare");
 		students.retainAll(children);
-
+		
 		PageData pageData = application.getApplicationData().getPageData("schoolDetails");
+		PageData schoolStartDatePageData = application.getApplicationData().getPageData("schoolStartDate");
+		PageData gradePageData = application.getApplicationData().getPageData("schoolGrade");
 
 		if (pageData == null) {
 			return List.of();
 		}
 		List<String> schoolNames = pageData.get("schoolName").getValue();
+		List<String> schoolStartDates = schoolStartDatePageData.get("schoolStartDate").getValue();
+		List<String> grades = gradePageData.get("schoolGrade").getValue();
 
 		List<DocumentField> result = new ArrayList<>();
 
 		for (int i = 0; i < students.size(); i++) {
-
 			String fullName = students.get(i);
 			String schoolName = schoolNames.get(i);
+			String grade = grades.get(i);
 
 			result.add(new DocumentField("whoIsGoingToSchool", "fullName", List.of(fullName),
 					DocumentFieldType.SINGLE_VALUE, i));
 			result.add(new DocumentField("schoolDetails", "schoolName", List.of(schoolName),
 					DocumentFieldType.SINGLE_VALUE, i));
+			result.add(new DocumentField("schoolGrade", "schoolGrade", List.of(grade),
+					DocumentFieldType.SINGLE_VALUE, i));
+		}
+	
+		for (int i = 0; i < schoolStartDates.size(); i++) {
+			String element = schoolStartDates.get(i);
+            if (element.length() == 1) {
+            	schoolStartDates.set(i, "0" + element);
+            }
+		}		
+			for (int i = 0, j=0;  i < schoolStartDates.size(); j++, i+=3) {
+				
+		        List<String> sublist = schoolStartDates.subList(i, Math.min(i+3, schoolStartDates.size())); // ["", "", ""]
+		        boolean allEmpty = sublist.stream().allMatch(String::isEmpty);
+		        String sublistString;
+
+		        if (allEmpty) {
+		            sublistString = "";
+		        } else {
+		            for (int k = 0; k < sublist.size(); k++) {
+		                if (sublist.get(k).isEmpty()) {
+		                    sublist.set(k, "-");
+		                }
+		            }
+		            sublistString = String.join("/", sublist);
+		        }
+
+
+			result.add(new DocumentField("schoolStartDate", "schoolStartDate", List.of(sublistString),
+					DocumentFieldType.SINGLE_VALUE, j));
 		}
 		return result;
 
