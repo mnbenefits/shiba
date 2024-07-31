410c66ee7991a49e057da8259ee3934f97c81669
EricMN35Johnson <eric.m.johnson@state.mn.us>
2024-07-31 16:25:41 -0500

    WIP

--- src/main/java/org/codeforamerica/shiba/pages/WicRecommendationService.java
+++ src/main/java/org/codeforamerica/shiba/pages/WicRecommendationService.java
@@ -8,9 +8,9 @@ public class WicRecommendationService {
 	
 	public boolean showWicMessage(Application application) {
 		System.out.println("===================WicRecommendationService appdata start====================");
-		System.out.println(application.getApplicationData().getSubworkflows().get("household").size());
-		//application.getApplicationData().getSubworkflows().get("household").stream().peek(x -> System.out.println("x=|" + x + "|"));
-		application.getApplicationData().getSubworkflows().get("household").stream().forEach(x -> System.out.println(x.getPagesData()));
+		System.out.println("Household size = " + application.getApplicationData().getSubworkflows().get("household").size());
+
+		application.getApplicationData().getSubworkflows().get("household").stream().forEach(x -> System.out.println(x.getPagesData().getPage("householdMemberInfo").get("dobAsDate")));
 		System.out.println("===================WicRecommendationService appdata end====================");
 		System.out.println("============ WicRecommendationService returning true");
 		return true;
