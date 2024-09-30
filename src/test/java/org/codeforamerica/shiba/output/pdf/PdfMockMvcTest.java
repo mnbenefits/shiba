4a62218ea5f2c6c82a3f3505af04b606709c1a63
pwjmb05 <john.bisek@state.mn.us>
2024-09-30 09:27:36 -0500

    CCM 158480: Merge page 15 back into the CCAP body, 158983: Write test ccapPdfForCaseWorkerShouldHaveExpectedPages

--- src/test/java/org/codeforamerica/shiba/output/pdf/PdfMockMvcTest.java
+++ src/test/java/org/codeforamerica/shiba/output/pdf/PdfMockMvcTest.java
@@ -1,25 +1,36 @@
 package org.codeforamerica.shiba.output.pdf;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.codeforamerica.shiba.output.Document.CCAP;
 import static org.codeforamerica.shiba.output.Document.CERTAIN_POPS;
 import static org.codeforamerica.shiba.output.caf.CoverPagePreparer.CHILDCARE_WAITING_LIST_UTM_SOURCE;
 import static org.codeforamerica.shiba.testutilities.TestUtils.assertPdfFieldEquals;
+import static org.codeforamerica.shiba.testutilities.TestUtils.ADMIN_EMAIL;
 import static org.codeforamerica.shiba.testutilities.TestUtils.assertPdfFieldContains;
 import static org.codeforamerica.shiba.testutilities.TestUtils.assertPdfFieldIsEmpty;
 import static org.codeforamerica.shiba.testutilities.TestUtils.assertPdfFieldIsNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oauth2Login;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.zip.ZipInputStream;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import org.apache.pdfbox.text.PDFTextStripper;
 import org.codeforamerica.shiba.Program;
 import org.codeforamerica.shiba.output.Document;
 import org.codeforamerica.shiba.pages.data.InputData;
@@ -29,6 +40,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.ResultActions;
 
 @Tag("pdf")
 public class PdfMockMvcTest extends AbstractShibaMockMvcTest {
@@ -280,6 +292,85 @@ public class PdfMockMvcTest extends AbstractShibaMockMvcTest {
 
 		var ccap = submitAndDownloadCcap();
 		assertPdfFieldEquals("COUNTY_INSTRUCTIONS", expectedCountyInstructions, ccap);
+	}
+
+	// Verify that the CCAP PDF which is generated for the caseworker has all of the
+	// pieces\pages that it should have.
+	@Test
+	void ccapPdfForCaseWorkerShouldHaveExpectedPages() throws Exception {
+		// Run a simple CCAP flow
+		fillOutPersonalInfo();
+		selectPrograms("CCAP");
+		addHouseholdMembersWithProgram("NONE");
+		String jimHalpertId = getFirstHouseholdMemberId();
+		postExpectingSuccess("childrenInNeedOfCare", "whoNeedsChildCare",
+				List.of("Dwight Schrute applicant", "Jim Halpert " + jimHalpertId));
+		postExpectingSuccess("doYouHaveChildCareProvider", "hasChildCareProvider", "false");
+		postExpectingSuccess("whoHasParentNotAtHome", "whoHasAParentNotLivingAtHome", List.of("NONE_OF_THE_ABOVE"));
+		postExpectingSuccess("housingSubsidy", "hasHousingSubsidy", "false");
+		postExpectingSuccess("livingSituation", "livingSituation", "PAYING_FOR_HOUSING_WITH_RENT_LEASE_OR_MORTGAGE");
+		postExpectingSuccess("goingToSchool", "goingToSchool", "false");
+		postExpectingSuccess("pregnant", "isPregnant", "false");
+		postExpectingSuccess("migrantFarmWorker", "migrantOrSeasonalFarmWorker", "false");
+		postExpectingSuccess("usCitizen", "isUsCitizen", "true");
+		postExpectingSuccess("workSituation", "hasWorkSituation", "false");
+		postExpectingSuccess("tribalNationMember", "isTribalNationMember", "false");
+		postExpectingSuccess("employmentStatus", "areYouWorking", "false");
+		postExpectingSuccess("jobSearch", "currentlyLookingForJob", "false");
+		postExpectingSuccess("unearnedIncome", "unearnedIncome", List.of("NO_UNEARNED_INCOME_SELECTED"));
+		postExpectingSuccess("otherUnearnedIncome", "otherUnearnedIncome",
+				List.of("NO_OTHER_UNEARNED_INCOME_SELECTED"));
+		postExpectingSuccess("futureIncome", "additionalIncomeInfo", "");
+		postExpectingSuccess("medicalExpenses", "medicalExpenses", List.of("NONE_OF_THE_ABOVE"));
+		postExpectingSuccess("supportAndCare", "supportAndCare", "false");
+		postExpectingSuccess("assets", "assets", List.of("VEHICLE"));
+		postExpectingSuccess("savings", "haveSavings", "true");
+		postExpectingSuccess("liquidAssetsSingle", "liquidAssets", "500.00");
+		postExpectingSuccess("soldAssets", "haveSoldAssets", "false");
+		postExpectingSuccess("registerToVote", "registerToVote", "NO_ALREADY_REGISTERED");
+		postExpectingSuccess("healthcareCoverage", "healthcareCoverage", "YES");
+		postExpectingSuccess("authorizedRep", "helpWithBenefits", "false");
+		postExpectingSuccess("additionalInfo", "caseNumber", "");
+		postExpectingSuccess("raceAndEthnicity", "raceAndEthnicity", "WHITE");
+		postExpectingRedirect("legalStuff", Map.of("agreeToTerms", List.of("true"), "drugFelony", List.of()),
+				"signThisApplication");
+		postExpectingSuccess("signThisApplication", "applicantSignature", "Dwight Schrute");
+		// Submit the application
+		submitApplication();
+
+		// Download the CCAP, the version for a caseworker. Note: /download downloads a
+		// .zip file
+		ResultActions resultActions = mockMvc.perform(get("/download")
+				.with(oauth2Login().attributes(attrs -> attrs.put("email", ADMIN_EMAIL))).session(session));
+		byte[] downloadBytes = resultActions.andReturn().getResponse().getContentAsByteArray();
+		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(downloadBytes);
+		// The /download endpoint downloads a .zip file
+		ZipInputStream zipFile = new ZipInputStream(byteArrayInputStream);
+		List<File> zippedFiles = unzip(zipFile);
+
+		// Extract the CCAP file
+		File ccapFile = zippedFiles.stream().filter(file -> getDocumentType(file).equals(CCAP)).toList().get(0);
+		byte[] ccapBytes = FileUtils.readFileToByteArray(ccapFile);
+		// PDAcroForm pdAcroForm =
+		// Loader.loadPDF(FileUtils.readFileToByteArray(ccapFile)).getDocumentCatalog().getAcroForm();
+		PDDocument pdDocument = Loader.loadPDF(ccapBytes);
+
+		// The CCAP PDF should have 29 pages (based on viewing a real example)
+		int pageCount = pdDocument.getNumberOfPages();
+		assert (pageCount == 29);
+
+		// Strip out all text so that we can search it for specific strings
+		PDFTextStripper pdfStripper = new PDFTextStripper();
+		String text = pdfStripper.getText(pdDocument);
+
+		assertTrue(text.contains("Attached is a new MNbenefits Application")); // cover-pages.pdf
+		assertTrue(text.contains("Minnesota Child Care Assistance Program Application")); // ccap-body-caseworker-page1.pdf
+		assertTrue(text.contains("2. Family members")); // ccap-body.pdf
+		assertTrue(text.contains("Authorization to share information for fraud investigation and audits.")); // ccap-body.pdf
+		assertTrue(text.contains("Perjury and general declarations")); // ccap-body-perjury-and-general-declarations.pdf
+		assertTrue(text.contains("Use this space if you need additional room")); // ccap-body-additional-room.pdf
+
+		pdDocument.close();
 	}
 
 	@Nested
