ed3080c1e9b96b5a2c7946abaf01759ea9924487
pwjmb05 <john.bisek@state.mn.us>
2024-11-02 08:29:28 -0500

    CCM Task 162124: Implement UPLOAD_DOC cover page test

--- src/test/java/org/codeforamerica/shiba/pages/LaterDocsMockMvcTest.java
+++ src/test/java/org/codeforamerica/shiba/pages/LaterDocsMockMvcTest.java
@@ -7,18 +7,28 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.codeforamerica.shiba.County;
 import org.codeforamerica.shiba.ServicingAgencyMap;
 import org.codeforamerica.shiba.TribalNation;
 import org.codeforamerica.shiba.TribalNationRoutingDestination;
+import org.codeforamerica.shiba.application.Application;
 import org.codeforamerica.shiba.application.FlowType;
 import org.codeforamerica.shiba.mnit.CountyRoutingDestination;
 import org.codeforamerica.shiba.mnit.RoutingDestination;
 import org.codeforamerica.shiba.output.Document;
+import org.codeforamerica.shiba.output.pdf.PdfGenerator;
 import org.codeforamerica.shiba.testutilities.AbstractShibaMockMvcTest;
 import org.codeforamerica.shiba.testutilities.TestApplicationDataBuilder;
 import org.junit.jupiter.api.BeforeEach;
@@ -32,6 +42,9 @@ public class LaterDocsMockMvcTest extends AbstractShibaMockMvcTest {
 
 	private static final String UPLOADED_JPG_FILE_NAME = "shiba+file.jpg";
 
+	@Autowired
+	private PdfGenerator pdfGenerator;
+	
 	@Autowired
 	private RoutingDecisionService routingDecisionService;
 
@@ -86,6 +99,55 @@ public class LaterDocsMockMvcTest extends AbstractShibaMockMvcTest {
 		assertThat(routingDecisionService.getRoutingDestinations(applicationData, CAF))
 				.containsExactly(countyMap.get(County.getForName("Aitkin")));
 	}	
+
+	/*
+	 * This test verifies that the fields of the UPLOAD_DOC cover page are being
+	 * filled correctly.
+	 */
+	@Test
+	void coverPageForLaterDocsShouldHaveExpectedFields() throws Exception {
+		// generate the ApplicationData object
+		postExpectingSuccess("matchInfo", Map.of("firstName", List.of("Dwight"), "lastName", List.of("Schrute"),
+				"dateOfBirth", List.of("01", "12", "1928"), "ssn", List.of("111-55-2222"), "phoneNumber",
+				List.of("888-888-8888"), "email", List.of("someone@test.local"), "caseNumber", List.of("1234567")));
+		postExpectingSuccess("identifyCounty", "county", "Clearwater");
+		postExpectingSuccess("tribalNationMember", "isTribalNationMember", "True");
+		postExpectingSuccess("selectTheTribe", "selectedTribe", "Red Lake Nation");
+
+		// generate the Application object
+		ZonedDateTime completedDateTime = ZonedDateTime.of(2024, 10, 31, 12, 0, 0, 0, ZoneId.of("UTC"));
+		Application application = Application.builder().id(applicationData.getId()).applicationData(applicationData)
+				.county(County.Clearwater).completedAt(completedDateTime).flow(FlowType.LATER_DOCS).build();
+
+		// generate the cover page PDF - we don't need to merge it with actual upload
+		// documents
+		byte[] coverPageBytes = pdfGenerator.generateCoverPageForUploadedDocs(application);
+		Path path = Files.createTempDirectory("");
+		File file = new File(path.toFile(), "testfile.pdf");
+		String absPath = file.getAbsolutePath();
+		Files.write(file.toPath(), coverPageBytes);
+
+		// use PdfBox to get extract the AcroForm
+		PDDocument pdDocument = Loader.loadPDF(file);
+		PDAcroForm acroForm = pdDocument.getDocumentCatalog().getAcroForm();
+		
+		// clean up the temporary directory and file
+		file.delete();
+		Files.deleteIfExists(path);
+
+		// verify that the cover page has the expected fields and values
+		assertThat(acroForm.getField("APPLICATION_ID").getValueAsString()).isEqualTo(applicationData.getId());
+		assertThat(acroForm.getField("SUBMISSION_DATETIME").getValueAsString()).contains("10/31/2024");
+		assertThat(acroForm.getField("DOCUMENT_DESTINATIONS").getValueAsString())
+				.isEqualTo("Clearwater County and Red Lake Nation");
+		assertThat(acroForm.getField("FULL_NAME").getValueAsString()).isEqualTo("Dwight Schrute");
+		assertThat(acroForm.getField("DATE_OF_BIRTH").getValueAsString()).isEqualTo("01/12/1928");
+		assertThat(acroForm.getField("APPLICANT_SSN").getValueAsString()).isEqualTo("111-55-2222");
+		assertThat(acroForm.getField("APPLICANT_PHONE_NUMBER").getValueAsString()).isEqualTo("888-888-8888");
+		assertThat(acroForm.getField("APPLICANT_EMAIL").getValueAsString()).isEqualTo("someone@test.local");
+		assertThat(acroForm.getField("CASE_NUMBER").getValueAsString()).isEqualTo("1234567");
+		assertThat(acroForm.getField("TRIBAL_NATION").getValueAsString()).isEqualTo("Red Lake Nation");
+	}
 
 	@Test
 	void routeLaterDocsToCorrectTribalNationOnly() throws Exception {
