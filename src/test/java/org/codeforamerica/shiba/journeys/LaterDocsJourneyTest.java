31cde92668ac5757fda22656773b85731bdd4e5c
Eric Johnson <eric.m.johnson@state.mn.us>
2025-05-02 07:49:36 -0500

    Added tests for back navigation.

--- src/test/java/org/codeforamerica/shiba/journeys/LaterDocsJourneyTest.java
+++ src/test/java/org/codeforamerica/shiba/journeys/LaterDocsJourneyTest.java
@@ -184,7 +184,7 @@ public class LaterDocsJourneyTest extends JourneyTest {
   //@Disabled("This test randomly fails on VDIs")
   @Test
   void laterDocsTribalNationFlow() {
-    testPage.clickButton("Upload documents");
+	  testPage.clickButton("Upload documents", "Ready to upload documents");
 
     assertThat(driver.getTitle()).isEqualTo("Ready to upload documents");
     assertNotNull(testPage.findElementById("headerMNbenefits"));
@@ -263,9 +263,9 @@ public class LaterDocsJourneyTest extends JourneyTest {
 
     uploadPdfFile();
     waitForDocumentUploadToComplete();
-    testPage.clickButton("Submit my documents");
+    testPage.clickButton("Submit my documents", "Doc submit confirmation");
     assertThat(driver.getTitle()).isEqualTo("Doc submit confirmation");
-    testPage.clickButton("No, add more documents"); // Go back
+    testPage.clickButton("No, add more documents", "Upload documents"); // Go back
     assertThat(driver.getTitle()).isEqualTo("Upload documents");
 
     testPage.clickButton("Submit my documents", "Doc submit confirmation");
@@ -282,5 +282,221 @@ public class LaterDocsJourneyTest extends JourneyTest {
     navigateTo("documentSubmitConfirmation");
     assertThat(driver.getTitle()).isEqualTo("Documents Sent");
   }
+  
+  @Test
+  void laterDocsMinimumFlowBrowserBackTest() {
+     testPage.clickButton("Upload documents", "Ready to upload documents");
+
+    assertThat(driver.getTitle()).isEqualTo("Ready to upload documents");
+    assertNotNull(testPage.findElementById("headerMNbenefits"));
+    assertNotNull(testPage.findElementById("footerMNbenefits"));
+    testPage.clickContinue("Match Info");
+
+    assertThat(driver.getTitle()).isEqualTo("Match Info");
+    // verify that the header & footer are the MNbenefits header & footer
+    assertNotNull(testPage.findElementById("headerMNbenefits"));
+    assertNotNull(testPage.findElementById("footerMNbenefits"));
+    // verify that the chat feature does exist on the MNbenefits header
+    assertFalse(testPage.elementDoesNotExistById("intercom_custom_launcher"));
+
+    testPage.enter("firstName", "defaultFirstName");
+    testPage.enter("lastName", "defaultLastName");
+    testPage.enter("ssn", "123456789");
+    testPage.enter("caseNumber", "1234567");
+    testPage.enter("phoneNumber", "7041234567");
+    testPage.clickContinue("Identify County");
+
+    // Identify County
+    assertThat(driver.getTitle()).isEqualTo("Identify County");
+    testPage.enter("county", "Hennepin");
+    testPage.clickContinue("Tribal Nation member");
+
+    // Tribal Nation Member
+    assertThat(driver.getTitle()).isEqualTo("Tribal Nation member");
+    testPage.clickButton("No", "How to add documents");
+
+    assertThat(driver.getTitle()).isEqualTo("How to add documents");
+    testPage.clickContinue("Upload documents");
+    
+    // should allow me to upload documents and those documents should be sent to the ESB
+    assertThat(driver.getTitle()).isEqualTo("Upload documents");
+    assertNotNull(testPage.findElementById("headerMNbenefits"));
+    assertNotNull(testPage.findElementById("footerMNbenefits"));
+    assertThat(driver.findElements(By.className("reveal")).size()).isEqualTo(0);
+
+    uploadPdfFile();
+    waitForDocumentUploadToComplete();
+    testPage.clickButton("Submit my documents", "Doc submit confirmation");
+    assertThat(driver.getTitle()).isEqualTo("Doc submit confirmation");
+    testPage.clickButton("No, add more documents", "Upload documents"); // Go back
+    assertThat(driver.getTitle()).isEqualTo("Upload documents");
+
+    testPage.clickButton("Submit my documents", "Doc submit confirmation");
+    testPage.clickButton("Yes, submit and finish", "Documents Sent");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    assertNotNull(testPage.findElementById("headerMNbenefits"));
+    assertNotNull(testPage.findElementById("footerMNbenefits"));
+    verify(pageEventPublisher).publish(any());
+
+    // Assert that applicant can't go back to any previous pages or resubmit docs at this point.
+   // Back navigation from the stack of pages in the browser history get called in backwards order.
+    navigateTo("documentSubmitConfirmation");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    navigateTo("uploadDocuments");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+  
+    navigateTo("howToAddDocuments");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    navigateTo("tribalNationMember");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    navigateTo("identifyCounty");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    navigateTo("matchInfo");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    navigateTo("readyToUploadDocuments");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    navigateTo("landing");
+    assertThat(driver.getTitle()).isEqualTo("MNbenefits"); 
+  }
+  
+  @Test
+  void laterDocsTribalNationFlowBrowserBackTest() {
+	  testPage.clickButton("Upload documents", "Ready to upload documents");
+
+    assertThat(driver.getTitle()).isEqualTo("Ready to upload documents");
+    assertNotNull(testPage.findElementById("headerMNbenefits"));
+    assertNotNull(testPage.findElementById("footerMNbenefits"));
+    testPage.clickContinue("Match Info");
+
+    assertThat(driver.getTitle()).isEqualTo("Match Info");
+    // verify that the header & footer are the MNbenefits header & footer
+    assertNotNull(testPage.findElementById("headerMNbenefits"));
+    assertNotNull(testPage.findElementById("footerMNbenefits"));
+    // verify that the chat feature does exist on the MNbenefits header
+    assertFalse(testPage.elementDoesNotExistById("intercom_custom_launcher"));
+
+    testPage.enter("firstName", "defaultFirstName");
+    testPage.enter("lastName", "defaultLastName");
+    testPage.enter("ssn", "123456789");
+    testPage.enter("caseNumber", "1234567");
+    testPage.enter("phoneNumber", "7041234567");
+    testPage.clickContinue("Identify County");
+
+    // Identify County
+    assertThat(driver.getTitle()).isEqualTo("Identify County");
+    testPage.enter("county", "Hennepin");
+    testPage.clickContinue("Tribal Nation member");
+
+    assertThat(driver.getTitle()).isEqualTo("Tribal Nation member");
+    testPage.clickButton("Yes", "Select a Tribal Nation");
+    assertThat(driver.getTitle()).isEqualTo("Select a Tribal Nation");
+    testPage.enter("selectedTribe", "Red Lake Nation");
+
+    testPage.clickContinue("How to add documents");
+    assertThat(driver.getTitle()).isEqualTo("How to add documents");
+    testPage.goBack();
+    testPage.goBack();
+    testPage.goBack();
+    testPage.enter("county", "Clearwater");
+    testPage.clickContinue("Tribal Nation member");
+    assertThat(driver.getTitle()).isEqualTo("Tribal Nation member");
+    testPage.clickButton("Yes", "Select a Tribal Nation");
+    
+    assertThat(driver.getTitle()).isEqualTo("Select a Tribal Nation");
+    testPage.enter("selectedTribe", "Red Lake Nation");
+    testPage.clickContinue("Nations Boundary");
+
+    assertThat(driver.getTitle()).isEqualTo("Nations Boundary");
+    testPage.clickButton("Yes", "Tribal Nation of residence");
+    testPage.enter("selectedNationOfResidence", "Red Lake Nation");
+    assertThat(testPage.getHeader()).isEqualTo("Tell us which Tribal Nation you live in.");
+    testPage.clickContinue("How to add documents");
+    assertThat(driver.getTitle()).isEqualTo("How to add documents");
+
+    // when White Earth Nation is selected, Nations Boundary is not displayed
+    testPage.goBack();
+    testPage.goBack();
+    testPage.goBack();
+    testPage.enter("selectedTribe", "White Earth Nation");
+    testPage.clickContinue("How to add documents");
+    assertThat(driver.getTitle()).isEqualTo("How to add documents");
+
+    //when the answer to live in nationsBoundary question is No, then next page is HowToAddDocuments
+    testPage.goBack();
+    testPage.enter("selectedTribe", "Red Lake Nation");
+    testPage.clickContinue("Nations Boundary");
+    //at this point we should be on the nations boundary page
+    testPage.clickButton("No", "How to add documents");
+    
+    // How to add documents
+    assertThat(driver.getTitle()).isEqualTo("How to add documents");
+    testPage.clickContinue("Upload documents");
+    
+    // should allow me to upload documents and those documents should be sent to the ESB
+    assertThat(driver.getTitle()).isEqualTo("Upload documents");
+    assertNotNull(testPage.findElementById("headerMNbenefits"));
+    assertNotNull(testPage.findElementById("footerMNbenefits"));
+    assertThat(driver.findElements(By.className("reveal")).size()).isEqualTo(0);
+
+    uploadPdfFile();
+    waitForDocumentUploadToComplete();
+    testPage.clickButton("Submit my documents", "Doc submit confirmation");
+    assertThat(driver.getTitle()).isEqualTo("Doc submit confirmation");
+    testPage.clickButton("No, add more documents", "Upload documents"); // Go back
+    assertThat(driver.getTitle()).isEqualTo("Upload documents");
+
+    testPage.clickButton("Submit my documents", "Doc submit confirmation");
+    testPage.clickButton("Yes, submit and finish", "Documents Sent");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    assertNotNull(testPage.findElementById("headerMNbenefits"));
+    assertNotNull(testPage.findElementById("footerMNbenefits"));
+    verify(pageEventPublisher).publish(any());
+
+    // Assert that applicant can't go back to previous pages or resubmit docs at this point.
+    // Back navigation from the stack of pages in the browser history get called in backwards order.
+    navigateTo("documentSubmitConfirmation");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    navigateTo("uploadDocuments");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    navigateTo("documentSubmitConfirmation");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    navigateTo("uploadDocuments");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    navigateTo("howToAddDocuments");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    navigateTo("nationsBoundary");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    navigateTo("selectTheTribe");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    navigateTo("tribalNationMember");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    navigateTo("identifyCounty");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    navigateTo("matchInfo");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    navigateTo("readyToUploadDocuments");
+    assertThat(driver.getTitle()).isEqualTo("Documents Sent");
+    
+    navigateTo("landing");
+    assertThat(driver.getTitle()).isEqualTo("MNbenefits"); 
+
+  }
+  
   
 }
