20ad21527bd20e634dc65936178159adf1040477
abdumn <abdu.esmael@state.mn.us>
2025-08-21 12:05:41 -0500

    CCM Task 198326: Update Percy testing to include unearnedIncome and otherUnearnedIncome

--- src/test/java/org/codeforamerica/shiba/testutilities/PercyTestPage.java
+++ src/test/java/org/codeforamerica/shiba/testutilities/PercyTestPage.java
@@ -6,9 +6,25 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class PercyTestPage extends Page {
 
   protected final Percy percy;
+  
 
   public PercyTestPage(RemoteWebDriver driver) {
     super(driver);
     this.percy = new Percy(driver);
   }
+
+  public void clickLink(String linkText, String nextPage) {
+	      percy.snapshot(driver.getTitle());
+	      super.clickLink(linkText, nextPage);
+	    }
+	  
+		public void clickButton(String buttonText, String nextPage) {
+	      percy.snapshot(driver.getTitle());
+	      super.clickButton(buttonText, nextPage);
+	    }
+	  
+		public void clickButtonLink(String buttonLinkText, String nextPage) {
+	      percy.snapshot(driver.getTitle());
+	      super.clickButtonLink(buttonLinkText, nextPage);
+	    }
 }
