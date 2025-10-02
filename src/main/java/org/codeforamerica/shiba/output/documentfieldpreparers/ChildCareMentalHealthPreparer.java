b9a9ac4d782ac86a8b83f521aada8ed0e687d8d4
abdumn <abdu.esmael@state.mn.us>
2025-10-02 14:47:09 -0500

    CCM Task 206704: Create the DocumentField preparer

---
+++ src/main/java/org/codeforamerica/shiba/output/documentfieldpreparers/ChildCareMentalHealthPreparer.java
@@ -0,0 +1,58 @@
+package org.codeforamerica.shiba.output.documentfieldpreparers;
+
+import static org.codeforamerica.shiba.output.DocumentFieldType.SINGLE_VALUE;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.codeforamerica.shiba.application.Application;
+import org.codeforamerica.shiba.output.Document;
+import org.codeforamerica.shiba.output.DocumentField;
+import org.codeforamerica.shiba.output.Recipient;
+import org.codeforamerica.shiba.pages.data.InputData;
+import org.codeforamerica.shiba.pages.data.PageData;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChildCareMentalHealthPreparer implements DocumentFieldPreparer {
+	List<DocumentField> fields = new ArrayList<>();
+
+	@Override
+	public List<DocumentField> prepareDocumentFields(Application application, Document document, Recipient recipient) {
+		fields = new ArrayList<DocumentField>();
+
+		PageData whoNeedsCare = application.getApplicationData().getPageData("whoNeedsChildCareForMentalHealth");
+		if (whoNeedsCare != null) {
+			InputData whoNeedsCareInputData = whoNeedsCare.get("whoNeedsChildCareMentalHealth");
+			if (whoNeedsCareInputData != null && !whoNeedsCareInputData.getValue().isEmpty()) {
+				List<String> listOfWhoNeedsCare = whoNeedsCareInputData.getValue();
+
+				for (int i = 0; i < listOfWhoNeedsCare.size(); i++) {
+					String namewithId = listOfWhoNeedsCare.get(i);
+					String fullName = namewithId.substring(0, namewithId.lastIndexOf(' '));
+					fields.add(new DocumentField("whoNeedsChildCareForMentalHealth", "fullName", fullName, SINGLE_VALUE,
+							i));
+				}
+			}
+
+		}
+
+		PageData timesData = application.getApplicationData().getPageData("childCareMentalHealthTimes");
+		if (timesData != null) {
+			InputData hours = timesData.get("childCareMentalHealthHours");
+			if (hours != null && !hours.getValue().isEmpty()) {
+				List<String> hoursValues = hours.getValue();
+				for (int i = 0; i < hoursValues.size(); i++) {
+					String hoursValue = hoursValues.get(i);
+					if (hoursValue != null && !hoursValue.trim().isEmpty()) {
+						fields.add(new DocumentField("childCareMentalHealthTimes", "childCareMentalHealthHours",
+								hoursValue, SINGLE_VALUE, i));
+					}
+				}
+
+			}
+		}
+		return fields;
+	}
+
+}
