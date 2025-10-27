2d339387710e4483944ed27596e07b86b9c097f2
eu01237926 <eu01237926@H55DEV1DSP1153.STATE.MN.GOV>
2025-10-27 10:57:47 -0500

    CCM Task 212113:Change the name of the usCitizen page to citizenship: Accessibility test fix

--- src/test/java/org/codeforamerica/shiba/journeys/AccessibilityJourneyTest.java
+++ src/test/java/org/codeforamerica/shiba/journeys/AccessibilityJourneyTest.java
@@ -350,7 +350,8 @@ public class AccessibilityJourneyTest extends JourneyTest {
     testPage.enter("whoIsPregnant", "Me");
     testPage.clickContinue("Expedited Migrant Farm Worker, Household");
     testPage.chooseYesOrNo("migrantOrSeasonalFarmWorker", NO.getDisplayValue(), "Citizenship");
-    testPage.clickElementById("citizenshipStatus[]-1-NOT_CITIZEN");
+    testPage.clickElementById("citizenshipStatus[]-0-NOT_CITIZEN");
+    testPage.clickElementById("citizenshipStatus[]-1-BIRTH_RIGHT");
     testPage.clickContinue("Disability");
     testPage.chooseYesOrNo("hasDisability", NO.getDisplayValue(), "Work changes");
     testPage.enter("workChanges", "None of the above");
