28e828a52f004268864b5e048b5a9042b47852b7
alexcolestatemn <alex.cole@state.mn.us>
2025-12-05 16:15:01 -0600

    CCM 216598 New Shared Test for studentFinancialAid and TaxCredit

---
+++ src/test/java/org/codeforamerica/shiba/pages/StudentFinancialTaxCreditTest.java
@@ -0,0 +1,44 @@
+package org.codeforamerica.shiba.pages;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.util.List;
+import java.util.Map;
+import org.codeforamerica.shiba.testutilities.AbstractShibaMockMvcTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class StudentFinancialTaxCreditTest extends AbstractShibaMockMvcTest {
+	
+	  @BeforeEach
+	  protected void setUp() throws Exception {
+	    super.setUp();
+	    mockMvc.perform(get("/pages/identifyCountyBeforeApplying").session(session)); // start timer
+	    postExpectingSuccess("identifyCountyBeforeApplying", "county", "Hennepin");
+	    postExpectingSuccess("writtenLanguage", Map.of("writtenLanguage", List.of("ENGLISH")));
+	    postExpectingSuccess("spokenLanguage", Map.of("spokenLanguage", List.of("ENGLISH")));
+	  }
+	  
+	  @Test
+	  void shouldFlowFromOtherUnearnedIncomeToStudentFinancialAid() throws Exception {
+		  selectPrograms("CASH");
+		  postExpectingRedirect("otherUnearnedIncome", "otherUnearnedIncome", "NO_OTHER_UNEARNED_INCOME_SELECTED", "studentFinancialAid");
+	  }
+	  
+	  @Test
+	  void shouldFlowFromOtherUnearnedIncomeSourcesToStudentFinancialAid() throws Exception {
+		  selectPrograms("CASH");
+		  postExpectingSuccess("otherUnearnedIncome", "otherUnearnedIncome", "OTHER_PAYMENTS");
+		  postExpectingRedirect("otherUnearnedIncomeSources", "otherUnearnedIncome", "OTHER_PAYMENTS", "studentFinancialAid");
+	  }
+	  
+	  @Test
+	  void shouldFlowFromOtherUnearnedIncomeSourcePagesToStudentFinancialAid() throws Exception {
+		  selectPrograms("CASH");
+		  fillOutPersonalInfo();
+		  addHouseholdMembersWithProgram("CASH");
+		  postExpectingRedirect("otherUnearnedIncome", "otherUnearnedIncome", List.of("DAY_TRADING", "OTHER_PAYMENTS"), "dayTradingIncomeSource");
+		  postExpectingRedirect("dayTradingIncomeSource", Map.of("monthlyIncomeDayTradingProceeds", List.of("Dwight Schrute applicant"), "dayTradingProceedsAmount", List.of("230", "")), "otherPaymentsIncomeSource");
+	  }
+
+}
