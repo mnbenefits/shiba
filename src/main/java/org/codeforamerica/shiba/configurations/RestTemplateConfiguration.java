d1b61819bd3ea0a1112995a6f00dbd7746d83d60
pwjmb05 <john.bisek@state.mn.us>
2026-02-24 10:48:25 -0600

    CCM Task 219346: Migrate to Spring Boot 4 (simplify RestTemplateConfiguration change)

--- src/main/java/org/codeforamerica/shiba/configurations/RestTemplateConfiguration.java
+++ src/main/java/org/codeforamerica/shiba/configurations/RestTemplateConfiguration.java
@@ -9,6 +9,8 @@ import java.security.cert.CertificateException;
 
 import javax.net.ssl.SSLContext;
 
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.apache.hc.client5.http.ssl.DefaultClientTlsStrategy;
@@ -18,7 +20,7 @@ import org.apache.hc.core5.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -32,11 +34,11 @@ public class RestTemplateConfiguration {
   private String connectTimeout;
   @Value("${client.read-timeout}")
   private String readTimeout;
+  @Bean
+  RestTemplate restTemplate() 
+	        throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, CertificateException, IOException {
 
-    @Bean
-    RestTemplate restTemplate()
-            throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, CertificateException, IOException {
-
+	    // Configure SSL context with trust configs
 	    SSLContext sslContext = SSLContextBuilder.create()
 	            .loadTrustMaterial(Paths.get(truststore).toFile(), truststorePassword.toCharArray())
 	            .build();
@@ -53,9 +55,17 @@ public class RestTemplateConfiguration {
 	    PoolingHttpClientConnectionManager connectionManager = connectionManagerbuilder.build();
 	    connectionManager.setDefaultMaxPerRoute(1);
 	    connectionManager.setMaxTotal(5);
-
-	    SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-	    requestFactory.setConnectTimeout(Integer.valueOf(connectTimeout));
+	    
+	    // Build HTTP client with connection manager
+	    CloseableHttpClient httpClient = HttpClientBuilder.create()
+	            .setConnectionManager(connectionManager)
+	            .build();
+	    
+	    // Create request factory with our custom HTTP client
+	    HttpComponentsClientHttpRequestFactory requestFactory = 
+	            new HttpComponentsClientHttpRequestFactory(httpClient);
+	    
+	    requestFactory.setConnectionRequestTimeout(Integer.valueOf(connectTimeout));
 	    requestFactory.setReadTimeout(Integer.valueOf(readTimeout));
 	    
 	    return new RestTemplate(requestFactory);
